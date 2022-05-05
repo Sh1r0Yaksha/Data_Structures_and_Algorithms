28a0327290d5631cd6624911d412cc00a6bcc85d
Shir0Yaksha <85480331+Sh1r0Yaksha@users.noreply.github.com>
2022-05-05 21:49:00 +0530

    BST

---
+++ DSA_Sem4/BinarySearchTrees.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Sem4
+{
+    internal class BinarySearchTrees
+    {      
+        public class Node
+        {
+            public int key;
+            public Node left;
+            public Node right;
+
+            public Node (int item)
+            {
+                key = item;
+                left = right = null;
+            }
+        }
+
+        public Node root;
+
+
+        public BinarySearchTrees()
+        {
+            root = null;
+        }
+
+        public void Insert (int key)
+        {
+            root = InsertRec(root, key);
+        }
+
+        Node InsertRec(Node root, int key)
+        {
+
+            // If the tree is empty,
+            // return a new node
+            if (root == null)
+            {
+                root = new Node(key);
+                return root;
+            }
+
+            // Otherwise, recur down the tree
+            if (key < root.key)
+                root.left = InsertRec(root.left, key);
+            else if (key > root.key)
+                root.right = InsertRec(root.right, key);
+
+            // Return the (unchanged) node pointer
+            return root;
+        }
+
+        public void Inorder()
+        {
+            InorderRec(root);
+        }
+
+        static void InorderRec(Node root)
+        {
+            if (root != null)
+            {              
+                InorderRec(root.left);
+
+                Console.WriteLine(root.key);
+
+                InorderRec(root.right);
+            }
+        }
+
+        public void PreOrder()
+        {
+            PreOrderRec(root);
+        }
+
+        void PreOrderRec(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            Console.WriteLine(root.key);
+
+            PreOrderRec(root.left);
+
+            PreOrderRec(root.right);
+                
+        }
+
+        public Node Search(Node root, int key)
+        {
+            if (root == null || root.key == key)
+                return root;
+
+            else if (root.key < key)
+                return Search(root.right, key);
+
+            else
+                return Search(root.left, key);
+        }
+    }
+
+    
+
+}
--- DSA_Sem4/Program.cs
+++ DSA_Sem4/Program.cs
@@ -36,6 +36,43 @@ namespace DSA_Sem4
             QuickSort.Sort(testArray, 0, testArray.Length - 1);
             PrintArray(testArray);
 
+            BinarySearchTrees bst = new BinarySearchTrees();
+            testArray = temp;
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                bst.Insert(testArray[i]);
+            }
+            Console.WriteLine("\n\nBST inorder traversal: ");
+            bst.Inorder();
+
+            Console.WriteLine("\n\nBST search for 11: ");
+
+            if (bst.Search(bst.root, 11) != null)
+                Console.WriteLine("It exists!");
+            else
+                Console.WriteLine("It doesn't exist");
+
+            Console.WriteLine("\n\nBST preorder traversal: ");
+            bst.PreOrder();
+
+            BinarySearchTrees bt = new BinarySearchTrees();
+            bt.root = new BinarySearchTrees.Node(38);
+            bt.root.left = new BinarySearchTrees.Node(28);
+            bt.root.right = new BinarySearchTrees.Node(48);
+            bt.root.left.left = new BinarySearchTrees.Node(23);
+            bt.root.left.right = new BinarySearchTrees.Node(33);
+            bt.root.left.left.left = new BinarySearchTrees.Node(13);
+            bt.root.left.left.right = new BinarySearchTrees.Node(26);
+            bt.root.right.left = new BinarySearchTrees.Node(43);
+            bt.root.right.right = new BinarySearchTrees.Node(58);
+            bt.root.right.right.left = new BinarySearchTrees.Node(53);
+            bt.root.right.right.right = new BinarySearchTrees.Node(68);
+
+            Console.WriteLine("\n\nBST preorder traversal: ");
+            bt.PreOrder();
+
+            Console.WriteLine("\n\nBST inorder traversal: ");
+            bt.Inorder();
         }
 
         static void PrintArray(int[] array)
