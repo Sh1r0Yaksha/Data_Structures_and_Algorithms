ec85e1ec9694e842c6c311ae79653e35d937a29c
Shir0Yaksha <85480331+Sh1r0Yaksha@users.noreply.github.com>
2022-05-06 18:32:06 +0530

    BST

--- DSA_Sem4/BinarySearchTrees.cs
+++ DSA_Sem4/BinarySearchTrees.cs
@@ -8,6 +8,7 @@ namespace DSA_Sem4
 {
     internal class BinarySearchTrees
     {      
+        // Creating nodes as classes, can be made as structures in functional languages like C
         public class Node
         {
             public int key;
@@ -21,76 +22,101 @@ namespace DSA_Sem4
             }
         }
 
+        // Assigning root node to the BST
         public Node root;
 
-
+        // Constructor for BST, used to call in programs
         public BinarySearchTrees()
         {
             root = null;
         }
 
+        // Method to call the insert function, key is the value to insert in the bst
         public void Insert (int key)
         {
-            root = InsertRec(root, key);
+            root = InsertRec(root, key); // Whenever Insert is called, it will run the insert method on root node
         }
 
+        // The method that inserts a key to the given root
         Node InsertRec(Node root, int key)
         {
 
-            // If the tree is empty,
-            // return a new node
+            // If the node doesn't exists, creates a new node and inserts the key value on the node
             if (root == null)
             {
                 root = new Node(key);
                 return root;
             }
 
-            // Otherwise, recur down the tree
-            if (key < root.key)
-                root.left = InsertRec(root.left, key);
-            else if (key > root.key)
-                root.right = InsertRec(root.right, key);
+            // If root node exists, checks if the key to add is greater than or less than the key of root, and places
+            // it accordingly to the left or right of the root node
+            else
+            {
+                if (key < root.key)
+                    root.left = InsertRec(root.left, key); // the new root node has become the left node if key is smaller
+                else if (key > root.key)
+                    root.right = InsertRec(root.right, key);// the new root node has become the right node if key is larger
 
-            // Return the (unchanged) node pointer
-            return root;
+                return root;
+            }
+            
         }
 
+        // Method to call the inorder traversal function
         public void Inorder()
         {
-            InorderRec(root);
+            InorderRec(root); // runs inorder traversal on root node of bst
         }
 
+        // The method prints the keys in inorder way, i.e. first prints the left node, then root node and then right node
         static void InorderRec(Node root)
         {
+            // Checks if root exists, then recursively runs the inorder traversal steps
             if (root != null)
             {              
                 InorderRec(root.left);
-
                 Console.WriteLine(root.key);
-
                 InorderRec(root.right);
             }
         }
 
+        // Method to call the preorder traversal function
         public void PreOrder()
         {
             PreOrderRec(root);
         }
 
+        // The method prints the keys in preorder way, i.e. first prints the root node, then left node and then right node
         void PreOrderRec(Node root)
         {
-            if (root == null)
+            // Checks if root exists, then recursively runs the preorder traversal steps
+            if (root != null)
             {
-                return;
-            }
-            Console.WriteLine(root.key);
-
-            PreOrderRec(root.left);
-
-            PreOrderRec(root.right);
-                
+                Console.WriteLine(root.key);
+                PreOrderRec(root.left);
+                PreOrderRec(root.right);
+            }                          
         }
 
+        // Method to call the postorder traversal function
+        public void PostOrder()
+        {
+            PostOrderRec(root);
+        }
+        // The method prints the keys in preorder way, i.e. first prints the left node, then right node and then root node
+        void PostOrderRec(Node root)
+        {
+            // Checks if root exists, then recursively runs the postorder traversal steps
+            if (root != null)
+            {
+                PostOrderRec(root.left);
+                PostOrderRec(root.right);
+                Console.WriteLine(root.key);
+            }            
+        }
+
+        // Method to search for a key in the BST, it is like the binary search operation where we compare the value to
+        // search with the key of root node and go left or rightacoording to value is s
         public Node Search(Node root, int key)
         {
             if (root == null || root.key == key)
--- DSA_Sem4/Program.cs
+++ DSA_Sem4/Program.cs
@@ -6,79 +6,93 @@ namespace DSA_Sem4
     {
         static void Main(string[] args)
         {
-            int[] testArray = {1,2,9,4,5,6,3,7,8};
-            int[] temp = testArray;
 
-            Console.WriteLine("Bubble sort: ");
+            // Initializing array
+            int[] testArray = {4,2,9,1,5,6,3,7,8};
+            int[] temp = new int[testArray.Length];
+            ReplaceArray(testArray, temp);
+
+            // Printing original array
+            Console.WriteLine("The original array: ");
+            PrintArray(testArray);
+            
+            // Bubble sort
+            Console.WriteLine("\n\nBubble sort: ");
             BubbleSort.BubbleSortMethod(testArray);
             PrintArray(testArray);
 
+            // Binary search
+            ReplaceArray(temp, testArray);
             Console.WriteLine("\n\nBinary search:");
-            Console.WriteLine(BinarySearch.BinarySearchMethod(9, testArray, 0, testArray.Length - 1));
+            Console.WriteLine(BinarySearch.BinarySearchMethod(2, testArray, 0, testArray.Length - 1));
 
-            testArray = temp;
+            // Insertion sort
+            ReplaceArray(temp, testArray);
             Console.WriteLine("\n\nInsertion sort:");
             InsertionSort.InsertionSortMethod(testArray);
             PrintArray(testArray);
 
-            testArray = temp;
+            // Merge sort
+            ReplaceArray(temp, testArray);
             Console.WriteLine("\n\nMergeSort: ");
             MergeSort.Sort(testArray, 0, testArray.Length - 1);
             PrintArray(testArray);
 
-            testArray = temp;
+            // Heap sort
+            ReplaceArray(temp, testArray);
             Console.WriteLine("\n\nHeapSort: ");
             HeapSort.Sort(testArray);
             PrintArray(testArray);
 
-            testArray = temp;
+            // Quick sort
+            ReplaceArray(temp, testArray);
             Console.WriteLine("\n\nQuickSort: ");
             QuickSort.Sort(testArray, 0, testArray.Length - 1);
             PrintArray(testArray);
 
+            // BST
             BinarySearchTrees bst = new BinarySearchTrees();
-            testArray = temp;
+            ReplaceArray(temp, testArray);
+
+            // Inserting elements in BST
             for (int i = 0; i < testArray.Length; i++)
             {
                 bst.Insert(testArray[i]);
             }
+
+
+            // BST Inorder traversal
             Console.WriteLine("\n\nBST inorder traversal: ");
             bst.Inorder();
 
-            Console.WriteLine("\n\nBST search for 11: ");
+            // BST Preorder traversal
+            Console.WriteLine("\n\nBST preorder traversal: ");
+            bst.PreOrder();
 
+            // BST Postorder traversal
+            Console.WriteLine("\n\nBST postorder traversal: ");
+            bst.PostOrder();
+
+            // BST search
+            Console.WriteLine("\n\nBST search for 11: ");
             if (bst.Search(bst.root, 11) != null)
                 Console.WriteLine("It exists!");
             else
                 Console.WriteLine("It doesn't exist");
-
-            Console.WriteLine("\n\nBST preorder traversal: ");
-            bst.PreOrder();
-
-            BinarySearchTrees bt = new BinarySearchTrees();
-            bt.root = new BinarySearchTrees.Node(38);
-            bt.root.left = new BinarySearchTrees.Node(28);
-            bt.root.right = new BinarySearchTrees.Node(48);
-            bt.root.left.left = new BinarySearchTrees.Node(23);
-            bt.root.left.right = new BinarySearchTrees.Node(33);
-            bt.root.left.left.left = new BinarySearchTrees.Node(13);
-            bt.root.left.left.right = new BinarySearchTrees.Node(26);
-            bt.root.right.left = new BinarySearchTrees.Node(43);
-            bt.root.right.right = new BinarySearchTrees.Node(58);
-            bt.root.right.right.left = new BinarySearchTrees.Node(53);
-            bt.root.right.right.right = new BinarySearchTrees.Node(68);
-
-            Console.WriteLine("\n\nBST preorder traversal: ");
-            bt.PreOrder();
-
-            Console.WriteLine("\n\nBST inorder traversal: ");
-            bt.Inorder();
         }
 
         static void PrintArray(int[] array)
         {
             foreach (int i in array)
                 Console.WriteLine(i);
+        }
+
+        static void ReplaceArray(int[] sourceArray, int[] targetArray)
+        {
+            for (int i = 0; i < sourceArray.Length; i++)
+            {
+                targetArray[i] = sourceArray[i];
+            }
         }
     }
 }
